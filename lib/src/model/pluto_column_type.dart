7fc6012ac3ac35b03461f5c8106cb68052da88af
Kaushik Gupta <129219828+KaushikGupta007@users.noreply.github.com>
2025-03-28 19:33:43 +0530

    DecimalIntl in applyFormat changed

--- lib/src/model/pluto_column_type.dart
+++ lib/src/model/pluto_column_type.dart
@@ -586,7 +586,7 @@ mixin PlutoColumnTypeWithNumberFormat {
       number = 0;
     }
 
-    return numberFormat.format(DecimalIntl(Decimal.parse(number.toStringAsFixed(7))));
+    return numberFormat.format(DecimalIntl(Decimal.parse(number.toStringAsFixed(10))));
   }
 
   /// Convert [String] converted to [applyFormat] to [number].
